7ce618dd4ca2271eb0f4ca17b924fbe14280cffd
sbx_user1051 <sbx_user1051@169.254.136.237>
2019-09-19 06:02:40 +0000

    Thu Sep 19 06:02:40 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,21 @@
+from collections import namedtuple, Counter
 
+Price = namedtuple('Price', ('items', 'price'))
+
+prices = {
+    "A": [Price(3, 130), Price(1, 50)],
+    "B": [Price(2, 45), Price(1, 30)],
+    "C": [Price(1, 20)],
+    "D": [Price(1, 15)],
+}
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    counter = Counter()
+    for item in skus:
+        if item not in prices:
+            return -1
+
+        counter.
+
